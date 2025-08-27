891b64e19fba524945b913e4ecc19d0b36ac9c40
Asma Rasheed <Asmarasheed4599@gmail.com>
2025-08-28 00:10:43 +0300

    initial map setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import DroneMap from './components/DroneMap'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div style={{ display: "flex", height: "100vh", width: "100%", position: "relative" }}>
+      <DroneMap  />
+    </div>
   )
 }
 
