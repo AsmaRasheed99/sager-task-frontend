6e2a320a3fdb409ed6df02ceb9ef32c388be1f1f
Asma Rasheed <Asmarasheed4599@gmail.com>
2025-08-27 21:22:30 +0300

    connected socke

---
+++ client/src/types/drone.ts
@@ -0,0 +1,28 @@
+export interface DroneFeature {
+    type: "Feature";
+    properties:{
+        serial: string;
+        registration: string;
+        Name: string;
+        altitude: number;
+        pilot: string;
+        organization: string;
+        yaw: number;
+    };
+    geometry: {
+        type: "Point";
+        coordinates: [number, number];
+    };
+}
+
+export interface DroneData {
+    type : "FeatureCollection";
+    features: DroneFeature[];
+}
+
+// Extended type used in context for path history 
+export interface DroneWithPath {
+  serial: string;
+  properties: DroneFeature["properties"];
+  path: [number, number][]; // array of the history [lng, lat]
+}
