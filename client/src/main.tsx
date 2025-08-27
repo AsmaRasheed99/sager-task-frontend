bc53265d76c6f56451e90794c7e6c449562ff970
Asma Rasheed <Asmarasheed4599@gmail.com>
2025-08-28 00:11:45 +0300

    wrapped the app with the drone provider to be able to access drone data across the whole app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { DroneProvider } from './contexts/DroneContext';
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+<React.StrictMode>
+    <DroneProvider>
+      <App />
+    </DroneProvider>
+</React.StrictMode>
+);
