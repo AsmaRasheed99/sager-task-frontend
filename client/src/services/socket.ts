a212f9536edf93f51356e186ecd47c765446a5dc
Asma Rasheed <Asmarasheed4599@gmail.com>
2025-08-27 20:07:49 +0300

    connected socket

---
+++ client/src/services/socket.ts
@@ -0,0 +1,23 @@
+import { io, Socket } from "socket.io-client";
+
+const SOCKET_URL = "http://localhost:9013";
+
+let socket: Socket | null = null;
+
+// connect socket to server
+export const connectSocket = () =>{
+if (!socket) {
+    socket = io(SOCKET_URL, {
+        transports: ["polling"],
+    });
+}
+return socket;
+};
+
+// disconnect socket from server
+export const disconnectSocket = () => {
+    if (socket) {
+        socket.disconnect();
+        socket = null;
+    }
+};
